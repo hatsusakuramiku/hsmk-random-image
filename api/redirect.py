2d96e898f10c8df4da427fcf450f8582882a70e9
hatsusakuramiku <hcj13794190602@gmail.com>
2025-02-02 17:08:21 +0800

    update

--- api/redirect.py
+++
@@ -1,8 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(301)
-        self.send_header('Location', '/gacha')
-        self.end_headers()
-        return
